64c41daefc759b4a139a198ae8a735d6cec64598
Yanulio <yagshulepa@edu.hse.ru>
2021-10-15 11:15:44 +0300

    first upload

---
+++ module01/01seminar/class/1/1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace test8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strA = Console.ReadLine();
+            int m = 0;
+            int min = 10;
+            int c = 0;
+            int a = int.Parse(strA);
+            while (a > 0)
+            {
+                c = a % 10;
+                if (c < min)
+                    min = c;
+                if (c > m)
+                    m = c;
+                a = a / 10;
+            }
+            Console.WriteLine($"{m} {min}");
+        }
+    }
+}
